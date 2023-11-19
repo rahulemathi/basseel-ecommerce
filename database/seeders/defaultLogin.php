d33d7396eb6cdf4cfd2703c3f0f9f25f08048e67
rahulemathi <erahul096@gmail.com>
2023-11-19 20:42:34 +0530

    fixed some bugs

--- database/seeders/defaultLogin.php
+++ database/seeders/defaultLogin.php
@@ -18,7 +18,6 @@ class defaultLogin extends Seeder
             'name' => 'admin',
             'email'=>'admin@gmail.com',
             'role' => 'admin',
-            'phone' => '1323644',
             'password' => Hash::make('Admin@123')
         ]);
 
@@ -26,7 +25,6 @@ class defaultLogin extends Seeder
             'name' => 'ram',
             'email'=>'ram@gmail.com',
             'role' => 'seller',
-            'phone' => '13245644',
             'password' => Hash::make('ram@123')
         ]);
     }
